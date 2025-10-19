f4d317bedc877790a2e9b1fb87114704a737e8af
高艳兵 <yanbing.gao@aliyun.com>
2025-10-19 13:34:44 +0800

    feat： 处理多层嵌套的情况

--- packages/reactivity/src/baseHandlers.ts
+++ packages/reactivity/src/baseHandlers.ts
@@ -1,6 +1,7 @@
-import { hasChanged } from '@vue/shared';
+import { hasChanged, isObject } from '@vue/shared';
 import { track, trigger } from './dep';
 import { isRef } from './ref';
+import { reactive } from './reactive';
 
 export const mutableHandlers: ProxyHandler<object> = {
   get(target, key, receiver) {
@@ -16,6 +17,11 @@ export const mutableHandlers: ProxyHandler<object> = {
        * 如果target.a 是一个 ref，那么就直接把值给它，不要让它 .value
        */
       return res.value;
+    }
+
+    if (isObject(res)) {
+      // 处理对象的嵌套响应式问题；
+      return reactive(res);
     }
 
     // receiver 用来保证访问器里面的 this 指向代理对象；
