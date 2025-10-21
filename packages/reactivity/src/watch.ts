c12bab6faafa7b0f30d9ce0f4696e005ab9700ff
高艳兵 <yanbing.gao@aliyun.com>
2025-10-21 18:43:34 +0800

    feat: 初级deep

--- packages/reactivity/src/watch.ts
+++ packages/reactivity/src/watch.ts
@@ -1,6 +1,7 @@
 import { isRef, type Ref } from './ref';
 import type { ComputedRef } from './computed';
 import { ReactiveEffect } from './effect';
+import { isObject } from '@vue/shared';
 
 export type WatchSource<T = any> = Ref<T, any> | ComputedRef<T> | (() => T);
 
@@ -26,7 +27,7 @@ export interface WatchHandle extends WatchStopHandle {
  * watch 的实现原理：就是依赖effect的scheduler；
  */
 export function watch(source: any, cb?: Function, options: any = {}) {
-  const { immediate, once } = options;
+  const { immediate, once, deep } = options;
 
   if (once) {
     // 如果once为true，则需要包装一下回调函数，在回调函数执行后，自动调用cleanup；
@@ -41,6 +42,12 @@ export function watch(source: any, cb?: Function, options: any = {}) {
 
   if (isRef(source)) {
     getter = () => source.value;
+  }
+
+  if (deep) {
+    const baseGetter = getter;
+
+    getter = () => traverse(baseGetter());
   }
 
   let oldValue: any;
@@ -74,4 +81,19 @@ export function watch(source: any, cb?: Function, options: any = {}) {
   }
 
   return cleanup;
+}
+
+function traverse(value: any, seen: Set<any> = new Set()) {
+  // 不是对象，直接返回；
+  if (!isObject(value)) return value;
+  // 如果已经遍历过，直接返回；
+  if (seen.has(value)) return value;
+
+  // 添加到seen中，避免重复遍历；
+  seen.add(value);
+  for (const key in value) {
+    traverse(value[key], seen);
+  }
+
+  return value;
 }
